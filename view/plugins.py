01b815ac37456bbbb22a8f2ed0c149822fa7607b
luisdanielta <luisdaniel.ta@outlook.com>
2022-12-23 18:24:57 -0500

    update url

--- view/plugins.py
+++ view/plugins.py
@@ -3,6 +3,8 @@ from core.components import Table
 
 class Plugins(Base):
 
+    URL: str = "https://raw.githubusercontent.com/luisdanielta/file-manager-plugins/main/plugins.json"
+
     def __init__(self):
         super().__init__()
         self.title("Plugins")
